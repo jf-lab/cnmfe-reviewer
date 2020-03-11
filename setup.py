b9372fd26256b93e427852eef4a079db7e25c7c9
linamnt <10761918+linamnt@users.noreply.github.com>
2020-03-10 23:25:29 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup
 
 setup(name="cnmfereview",
       version='0.1',
-      description="Semi-automated reviewing tools for CNMF-E extracted cells",
+      description="Using automl tools to review for CNMF-E extracted cells",
       author="linamnt",
       license="MIT",
       packages=['cnmfereview'],
+      install_requires=[
+          'auto-sklearn>0.6.0',
+          'scikit-learn>0.21.0,<0.22.0'
+          ],
       zip_safe=False)
